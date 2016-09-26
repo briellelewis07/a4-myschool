fff4a4d9b6edaecf65f81d440b076c3fbb8a5a5d
briellelewis07 <briellelewis07@gmail.com>
2016-09-26 13:26:22 -0600

    Add files via upload

---
+++ my-code.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	$("p").hide();
+	$("#pic1").hide();
+	$("h1").click(function() {
+
+		$(this).next().fadeToggle(1000);
+
+	});
+
+	/*start of jq02 how to use buttons and selectors*/
+
+	$("#testbutton").click(function() {
+
+		/*$("#jq02").css("background-color","#006699");*/
+		$(".strong").css("background-color","#006699");
+
+	});
+
+});
+
